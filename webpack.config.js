e704a269d55c41d58b0e56639873586dab204b7c
MAHDIA001 <mahdianaiebi@gmail.com>
2022-10-11 23:46:49 +0500

    fix bugs

--- webpack.config.js
+++ webpack.config.js
@@ -27,10 +27,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },  
+      },
     ],
   },
   optimization: {
     runtimeChunk: 'single',
-  },    
+  },
 };
