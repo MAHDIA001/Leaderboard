f450a8d9fa28fbac3b111dbb6f0d61fe4e3718eb
MAHDIA001 <mahdianaiebi@gmail.com>
2022-10-11 23:37:52 +0500

    project-setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.css';
+import add from './add.js';
+add();
+const refrsh = document.querySelector('.refresh');
+refrsh.addEventListener('click', () => {
+  window.location.reload();
+});
